d1813d43e6290a4f55c4f6fe176a24dfe6c53e55
Richard Bailey <richard@richardbailey.xyz>
2021-08-31 00:02:38 +0100

    http handlers

--- bff/game-resource.go
+++ bff/game-resource.go
@@ -1,6 +1,10 @@
 package bff
 
 import (
+	"context"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 	pbGameengine "github.com/rwbailey/m-apis/game-engine/v1"
 	pbHighscore "github.com/rwbailey/m-apis/highscore/v1"
@@ -53,4 +57,52 @@ func NewGrpcGameEngineServiceClient(serverAddr string) (pbGameengine.GameEngineC
 	client := pbGameengine.NewGameEngineClient(conn)
 
 	return client, nil
+}
+
+func (gr *gameResource) SetHighScore(ctx *gin.Context) {
+	highScoreStr := ctx.Param("hs")
+	highScore, err := strconv.ParseFloat(highScoreStr, 64)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to parse high score")
+	}
+	gr.gameClient.SetHighScore(context.Background(), &pbHighscore.SetHighScoreRequest{
+		HighScore: highScore,
+	})
+}
+
+func (gr *gameResource) GetHighScore(ctx *gin.Context) {
+	resp, err := gr.gameClient.GetHighScore(context.Background(), &pbHighscore.GetHighScoreRequest{})
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get high score")
+		return
+	}
+	hs := strconv.FormatFloat(resp.GetHighScore(), 'e', -1, 64)
+	ctx.JSONP(200, gin.H{
+		"hs": hs,
+	})
+}
+
+func (gr *gameResource) GetSize(ctx *gin.Context) {
+	resp, err := gr.gameEngineClient.GetSize(context.Background(), &pbGameengine.GetSizeRequest{})
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get size")
+		return
+	}
+
+	ctx.JSONP(200, gin.H{
+		"size": resp.GetSize(),
+	})
+}
+
+func (gr *gameResource) SetScore(ctx *gin.Context) {
+	scoreStr := ctx.Param("score")
+	score, _ := strconv.ParseFloat(scoreStr, 64)
+
+	_, err := gr.gameEngineClient.SetScore(context.Background(), &pbGameengine.SetScoreRequest{
+		Score: score,
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to set score")
+		return
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/rwbailey/m-bff
 go 1.15
 
 require (
+	github.com/gin-gonic/gin v1.7.4
 	github.com/rs/zerolog v1.24.0
 	github.com/rwbailey/m-apis v0.0.0-20210830165755-2baefb5a744a
 	google.golang.org/grpc v1.40.0
